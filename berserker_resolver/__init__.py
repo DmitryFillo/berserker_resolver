7e963b7598aea4848cf17582a393b92103f6cea0
Dmitry Fillo <fillo@fillo.me>
2015-07-30 03:14:23 +0300

    extra dir added

--- berserker_resolver/__init__.py
+++ berserker_resolver/__init__.py
@@ -1,4 +1,4 @@
 from berserker_resolver.resolver import ThreadResolver as Resolver
-from berserker_resolver.extra import detect_tries
+from berserker_resolver.extra.detect_tries import detect_tries
 
 __all__ = ['Resolver', 'detect_tries',]
